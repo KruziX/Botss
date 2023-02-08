7bffd31fb155c600bfbcc7a4d65fd86508132e63
KruzariuM <90509328+KruziX@users.noreply.github.com>
2023-02-08 19:16:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-# Слито в https://t.me/HACKER_PHONE_VIP
-
-TOKEN = '5582105803:AAHsqg6vQSBvaRiwKPYoiyL8L6iduo-tu_0' # Токен бота с @BotFather
-
-# Слито в https://t.me/HACKER_PHONE_VIP
+TOKEN = '5582105803:AAHoqm44U4YjQg2RR0zq9cC_m5vnZlNNQYM' # Токен бота с @BotFather
